4d9eb47f98124f13cb21a9a9217b2486d0631c94
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-03-05 15:13:17 -0800

    Filled the blanks

--- RepoWatcherWidget/Widget Sizes/LargeWidget.swift
+++ RepoWatcherWidget/Widget Sizes/LargeWidget.swift
@@ -9,16 +9,24 @@ import WidgetKit
 import SwiftUI
 
 struct LargeWidgetProvider: TimelineProvider {
+    
     func placeholder(in context: Context) -> LargeWidgetEntry {
-        <#code#>
+        LargeWidgetEntry(date: .now)
     }
     
     func getSnapshot(in context: Context, completion: @escaping (LargeWidgetEntry) -> Void) {
-        <#code#>
+        let entry = LargeWidgetEntry(date: .now)
+        completion(entry)
     }
     
     func getTimeline(in context: Context, completion: @escaping (Timeline<LargeWidgetEntry>) -> Void) {
-        <#code#>
+        
+        let nextUpdate = Date().addingTimeInterval(43_200) // 12 hours = 43,200 seconds
+        
+        let entry = LargeWidgetEntry(date: .now)
+        let timeline = Timeline(entries: [entry], policy: .after(nextUpdate))
+        
+        completion(timeline)
     }
 }
 
