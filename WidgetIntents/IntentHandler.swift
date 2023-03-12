36962bcfc5257581e51b44bc8ca91aee0ba3a7a3
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-03-11 22:51:04 -0800

    Added provideRepoCollection func

--- WidgetIntents/IntentHandler.swift
+++ WidgetIntents/IntentHandler.swift
@@ -9,11 +9,24 @@ import Intents
 
 class IntentHandler: INExtension {
     
-    override func handler(for intent: INIntent) -> Any {
-        // This is the default implementation.  If you want different objects to handle different intents,
-        // you can override this and return the handler you want for that particular intent.
-        
-        return self
-    }
+    //    override func handler(for intent: INIntent) -> Any {
+    //        // This is the default implementation.  If you want different objects to handle different intents,
+    //        // you can override this and return the handler you want for that particular intent.
+    //
+    //        return self
+    //    }
     
+    func provideRepoCollection(for intent: RepoIntent, with completion: @escaping (INObjectCollection<Repo>?, Error?) -> Void) {
+        
+        let repos: [Repo] = [
+            Repo(identifier: "SwiftUIBuddy", display: "SwiftUIBuddy"),
+            Repo(identifier: nil, display: "Setting")
+        ]
+        
+        // Create a collection with the array of characters.
+        let collection = INObjectCollection(items: repos)
+        
+        // Call the completion handler, passing the collection.
+        completion(collection, nil)
+    }
 }
