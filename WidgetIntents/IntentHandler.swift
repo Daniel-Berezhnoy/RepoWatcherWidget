ffcc3d4a4ed075f8b15d2d8cb0fad343d617c658
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-03-11 23:04:23 -0800

    Added provideSelectedRepoOptionsCollection function!

--- WidgetIntents/IntentHandler.swift
+++ WidgetIntents/IntentHandler.swift
@@ -6,20 +6,38 @@
 //
 
 import Intents
+//import WidgetIntents
+//import IntentsUI
+//import AppIntents
+//import WidgetKit
+//import SwiftUI
 
-class IntentHandler: INExtension {
+class IntentHandler: INExtension, RepoIntentHandling {
     
-    func provideRepoCollection(for intent: RepoIntent, with completion: @escaping (INObjectCollection<Repo>?, Error?) -> Void) {
-        
+    func provideSelectedRepoOptionsCollection(for intent: RepoIntent, searchTerm: String?, with completion: @escaping (INObjectCollection<Repo>?, Error?) -> Void) {
         let repos: [Repo] = [
             Repo(identifier: "SwiftUIBuddy", display: "SwiftUIBuddy"),
             Repo(identifier: nil, display: "Setting")
         ]
-        
+
         // Create a collection with the array of characters.
         let collection = INObjectCollection(items: repos)
-        
+
         // Call the completion handler, passing the collection.
         completion(collection, nil)
     }
+    
+    // MARK: Async version of the function
+//    func provideSelectedRepoOptionsCollection(for intent: RepoIntent, searchTerm: String?) async throws -> INObjectCollection<Repo> {
+//        let repos: [Repo] = [
+//            Repo(identifier: "SwiftUIBuddy", display: "SwiftUIBuddy"),
+//            Repo(identifier: nil, display: "Setting")
+//        ]
+//
+//        // Create a collection with the array of characters.
+//        let collection = INObjectCollection(items: repos)
+//
+//        // Return the collection.
+//        return collection
+//    }
 }
