47d43e9ecec9eefcbf9929fb1c7c22b6a839daf1
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2023-03-19 14:54:41 -0700

    Added configuration for the Large Widget

--- RepoWatcherWidget/Widgets/LargeWidget.swift
+++ RepoWatcherWidget/Widgets/LargeWidget.swift
@@ -8,27 +8,27 @@
 import WidgetKit
 import SwiftUI
 
-#warning("Implement Intensts Here")
-struct LargeWidgetProvider: TimelineProvider {
-    
-    let repoToShow = NetworkManager.shared.selectedRepoURL
+struct LargeWidgetProvider: IntentTimelineProvider {
     
     func placeholder(in context: Context) -> LargeWidgetEntry {
         LargeWidgetEntry(date: .now, repo: Repository.placeholder)
     }
     
-    func getSnapshot(in context: Context, completion: @escaping (LargeWidgetEntry) -> Void) {
+    func getSnapshot(for configuration: SelectRepoIntent, in context: Context, completion: @escaping (LargeWidgetEntry) -> Void) {
         let entry = LargeWidgetEntry(date: .now, repo: Repository.placeholder)
         completion(entry)
     }
     
-    func getTimeline(in context: Context, completion: @escaping (Timeline<LargeWidgetEntry>) -> Void) {
+    func getTimeline(for configuration: SelectRepoIntent, in context: Context, completion: @escaping (Timeline<LargeWidgetEntry>) -> Void) {
         Task {
             let nextUpdate = Date().addingTimeInterval(43_200) // 12 hours = 43,200 seconds
             
             do {
-                // Get Repo
+                // Get The Repo
+                let repoToShow = RepoURL.prefix + (configuration.repo ?? "Daniel-Berezhnoy/SwiftUIBuddy")
                 var repo = try await NetworkManager.shared.getRepo(from: repoToShow)
+                
+                // Download and set the Avatar for the Repo Owner
                 let avatarImageData = await NetworkManager.shared.downloadImageData(from: repo.owner.avatarUrl)
                 repo.avatarData = avatarImageData
                 
@@ -36,7 +36,7 @@ struct LargeWidgetProvider: TimelineProvider {
                 let contributors = try await NetworkManager.shared.getContributors(from: repoToShow + "/contributors")
                 var topContributors = Array(contributors.prefix(4))
                 
-                // Download the top 4 Avatars
+                // Download and set Avatars for the top 4 contributor
                 for i in topContributors.indices {
                     let avatarData = await NetworkManager.shared.downloadImageData(from: topContributors[i].avatarUrl)
                     topContributors[i].avatarData = avatarData
@@ -79,7 +79,8 @@ struct LargeWidget: Widget {
     let kind: String = "LargeWidget"
     
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind,
+        IntentConfiguration(kind: kind,
+                            intent: SelectRepoIntent.self,
                             provider: LargeWidgetProvider()) { entry in
             LargeWidgetEntryView(entry: entry)
         }
